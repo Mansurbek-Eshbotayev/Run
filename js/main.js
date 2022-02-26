c3fa3943beae55cb169022c649c6758588b0e317
Mansurbek-Eshbotayev <mustafoimron1820@gmail.com>
2022-02-26 07:52:52 +0500

    Add all change

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 var elForm = document.querySelector(".form");
 var elUserInput = document.querySelector(".form__input");
-var elResult1 = document.querySelector("result1");
-var elResult2 = document.querySelector("result2");
-var elResult2 = document.querySelector("result3");
-var elResult2 = document.querySelector("result4");
+var elResult1 = document.querySelector(".result1");
+var elResult2 = document.querySelector(".result2");
+var elResult3 = document.querySelector(".result3");
+var elResult4 = document.querySelector(".result4");
+
+function man(number,speed){
+    return number / speed;
+  }
 
 elForm.addEventListener("submit", function(e){
   e.preventDefault()
@@ -14,11 +18,12 @@ elForm.addEventListener("submit", function(e){
   var byCar =80;
   var byPlan =880;
   
-  function man(){
-    return elUserInputVal / manSpeed;
-  }
+  
 
 
-  elResult1.textContent = Math.round (man()) + "soat" ;
+  elResult1.textContent = Math.round (man(elUserInputVal,manSpeed)) + "soat" ;
+  elResult2.textContent = Math.round (man(elUserInputVal,baySpeed)) + "soat" ;
+  elResult3.textContent = Math.round (man(elUserInputVal,byCar)) + "soat" ;
+  elResult4.textContent = Math.round (man(elUserInputVal,byPlan)) + "soat" ;
   
 })
